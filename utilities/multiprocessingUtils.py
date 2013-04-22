7c23f1d289012c3bc7b859ecc66697cd18a0f7a6
Dom De Re <domdere@domdere.com>
2013-04-23 09:36:12 +1000

    Child processes created with MultiprocessingUtils now ignore keyboard interrupts, so they will complete their assigned tasks, it is up to the parent process to handle the keyboard interrupt now and reign in their children

--- utilities/multiprocessingUtils.py
+++ utilities/multiprocessingUtils.py
@@ -2,6 +2,7 @@ import logUtils
 
 import logging
 import multiprocessing
+import signal
 import traceback
 from contextlib import closing
 
@@ -10,11 +11,19 @@ from contextlib import closing
 def childProcessMain(logConfig, logQueue, mainMethod, args=(), kwargs={}):
     # setup the logging:
     try:
+        # disable the SIGINT handler/KeyboardInterrupt,
+        # if a worker is working on a job, it shouldnt be interrupted,
+        # as often the entire point of these workers might be for
+        # Asynchronous IO, and the output could be corrupted,
+        # its up to the parent process to handle the keyboard interrupt and
+        # call in this child process by whatever means 'mainMethod' allows
+        signal.signal(signal.SIGINT, signal.SIG_IGN)
+
         logUtils.configureWorkerProcess(logConfig, logQueue)
 
         mainMethod(*args, **kwargs)
 
-    except (KeyboardInterrupt, SystemExit):
+    except SystemExit:
         raise
     except:
         print('Problem with Process: ')
@@ -26,6 +35,14 @@ def childProcessMain(logConfig, logQueue, mainMethod, args=(), kwargs={}):
 # but just doesnt need a "mainMethod" to run, once the initialiser is done, the Pool will manage
 # its lifetime
 def poolProcessInitialiser(logConfig, logQueue):
+    # disable the SIGINT handler/KeyboardInterrupt,
+    # if a worker is working on a job, it shouldnt be interrupted,
+    # as often the entire point of these workers might be for
+    # Asynchronous IO, and the output could be corrupted,
+    # its up to the parent process to handle the keyboard interrupt and
+    # call in the worker processes...
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+
     logUtils.configureWorkerProcess(logConfig, logQueue)
 
     logging.getLogger(__name__).info('Pool Process Initialised')
@@ -35,13 +52,20 @@ def poolProcessInitialiser(logConfig, logQueue):
 # If numProcesses is not provided, it will check the number of cores on the
 # machine and then spawn the number of processes to match.
 def createMultiprocessPool(numProcesses=None):
-    return multiprocessing.Pool(
-        processes=numProcesses,
-        initializer=poolProcessInitialiser,
-        initargs=(logUtils.logConfig, logUtils.logQueue))
+    if numProcesses == None:
+        return multiprocessing.Pool(
+            initializer=poolProcessInitialiser,
+            initargs=(logUtils.logConfig, logUtils.logQueue))
+    else:
+        return multiprocessing.Pool(
+            processes=numProcesses,
+            initializer=poolProcessInitialiser,
+            initargs=(logUtils.logConfig, logUtils.logQueue))
 
 def joinPool(pool):
     # stop them from receiving anymore jobs...
     pool.close()
     # now wait for them to join
     pool.join()
+    
+    return
